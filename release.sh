a39a4a048147ee934685d8553dcc8ac4916cb1c6
Dallas Morisette <morisett@purdue.edu>
2013-11-18 14:11:11 -0500

    initial commit for auto release script

---
+++ release.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+LANG=C
+COMMON_DIR="../Common/"
+COMMON_FILES=(FindLinearFit.m GetConstants.m GetMaterial.m ImportCSV.m SaveAsCSV.m)
+SRC_FILES=(example.m TranslateData.m)
+OTHER_FILES=(README LICENSE data.csv)
+
+files=()
+for fname in "${COMMON_FILES[@]}" 
+do
+	files+=("$COMMON_DIR$fname")
+done
+rm -rf tmp
+mkdir -p tmp/hilo
+mkdir tmp/hilo/processed
+cp ${files[*]} ${SRC_FILES[*]} ${OTHER_FILES[*]} tmp/hilo
+
+version=`git describe`
+sed 's/\$VERSION_STRING\$/'"$version"'/' <hilo.m >tmp/hilo/hilo.m
